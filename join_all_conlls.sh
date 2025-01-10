5cd043fcd228de01b78534af9096d123577263ad
Jan Rodríguez Miret <janrodmir@gmail.com>
2025-01-10 18:08:29 +0100

    bugfix: avoid joining conll files under .ipynb_checkpoints

--- join_all_conlls.sh
+++ join_all_conlls.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-find "$1/train" -name '*.conll' -type f | sort | xargs sed -s -e '$a\' > "$1/train.conll"
-find "$1/valid" -name '*.conll' -type f | sort | xargs sed -s -e '$a\' > "$1/validation.conll"
-find "$1/test" -name "*.conll" -type f | sort | xargs sed -s -e '$a\' > "$1/test.conll"
+find "$1/train" -maxdepth 1 -name '*.conll' -type f | sort | xargs sed -s -e '$a\' > "$1/train.conll"
+find "$1/valid" -maxdepth 1 -name '*.conll' -type f | sort | xargs sed -s -e '$a\' > "$1/validation.conll"
+find "$1/test" -maxdepth 1 -name "*.conll" -type f | sort | xargs sed -s -e '$a\' > "$1/test.conll"
